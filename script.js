86a7d263427b6ebeaecd265e6f2480e2f54017b2
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-25 17:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
-    //Write your code here
+// Define a function to handle the form submission
+function getFormValue(event) {
+    event.preventDefault(); // Prevent the form from submitting in the traditional way
 
+    // Get the values of First and Last Name from the form
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+
+    // Display the values using the alert function
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
 }
+
+// Get a reference to the form element and attach an event listener to it
+var loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", getFormValue);
+
